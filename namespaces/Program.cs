c4221c9ba01f890ad7157ca2313c72cb1cb91888
ichibsah <info@skylinewebservice.de>
2016-05-22 22:08:26 +0200

    after sch

--- namespaces/Program.cs
+++ namespaces/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*namespace by alias*/
+using PATA = ProjectA.TeamA;
+using PATB = ProjectA.TeamB;
 
 namespace namespaces
 {
@@ -10,6 +13,34 @@ namespace namespaces
     {
         static void Main(string[] args)
         {
+            PATA.ClassA.Print();
+            PATB.ClassA.Print();
+        }
+    }
+
+}
+
+namespace ProjectA
+{
+    namespace TeamA
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team A Print Method");
+            }
+        }
+    }
+
+    namespace TeamB
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Team B Print Method");
+            }
         }
     }
 }
