a170e6bb7f35d59f7b751800be239797aa7b5956
ichibsah <info@skylinewebservice.de>
2016-05-26 23:18:39 +0200

    26.05.2016 23:18:39,24

---
+++ 021 - Inheritance/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _021___Inheritance
+{
+    public class Employee
+    {
+        string FirstName;
+        string Lastname;
+        string Email;
+
+        public void printFullName()
+        {
+            //
+        }
+
+        public Employee()
+        {
+    
+        }
+
+        public Employee( string message)
+        {
+
+        }
+
+    }
+
+    public class FullTimeEmployee : Employee
+    {
+        float YearlySalary;
+
+        public FullTimeEmployee() : base("derived class controlling parent class")
+        {
+    
+        }
+    
+    }
+
+
+
+    /*only 1 base class allowed*/
+    public class PartTimeEmplyee : Employee
+    {
+        float HourlyRate;
+    }
+
+    /*but multiple level inheritance is allowed*/
+    public class MultipleLevel : FullTimeEmployee
+    {
+        int Holidays;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FullTimeEmployee FTE = new FullTimeEmployee();
+            FTE.
+        }
+    }
+}
---
+++ 022 Method hiding/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Employee
+{
+    string FirstName;
+    string Lastname;
+    string Email;
+
+    public void printFullName()
+    {
+        //
+    }
+
+}
+public class FullTimeEmployee : Employee
+{
+    /*this will hide the class employee printFullName method. note the new keyword*/
+    public new void printFullName()
+    {
+        /*this will expicitly call the class employee printFullName method*/
+        base.printFullName();
+    }
+
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        FullTimeEmployee FTE = new FullTimeEmployee();
+        /*you can cast like here too to get FTE behave like an emplyee class*/
+        ((Employee)FTE).printFullName();
+
+        /*another method, you can change the object method like this*/
+        Employee ETE = new FullTimeEmployee();
+        ETE.printFullName();
+
+    }
+}
+
